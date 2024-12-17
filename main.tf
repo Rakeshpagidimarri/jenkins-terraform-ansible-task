cba70536f628163bf41feeeb8ab026d50d9866a2
Rakeshpagidimarri <rakeshpagidimarri8@outlook.com>
2024-12-17 20:31:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
-  ami                    = "ami-0fc5d935ebf8bc3bc"
+  ami                    = "ami-0e2c8caa4b6378d8c"
   instance_type          = "t2.micro" 
   key_name               = "rakesh"
   vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
@@ -23,7 +23,7 @@ EOF
 }
 
 resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
-  ami                    = "ami-05c13eab67c5d8861"
+  ami                    = "ami-01816d07b1128cd2d"
   instance_type          = "t2.micro"
   key_name               = "rakesh"
   vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
